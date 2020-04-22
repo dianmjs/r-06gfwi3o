c95fd2450c2ff7a29b31c35c58771e2801f7351a
dianmjs <dianamjs69@gmail.com>
2020-04-22 11:29:11 -0500

    prueba

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-import Welcome from './Welcome';
-import Setup from './Setup';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import Welcome from "./Welcome.js";
+import Setup from "./Setup";
 
-it('renders App without crashing', () => {
+it("renders App without crashing", () => {
   shallow(<App />);
 });
 
-it('renders three Welcome components', () => {
+it("renders three Welcome components", () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(Welcome).length).toBe(3);
 });
